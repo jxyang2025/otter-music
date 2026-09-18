e41d08b282aa434c5b16f8d727c1276e27e9421d
jxyang2025 <jxy5460@gmail.com>
2026-09-18 15:10:09 +0800

    本地文件加入歌单

--- android/app/src/main/java/com/otterhub/music/LocalMusicPlugin.java
+++ android/app/src/main/java/com/otterhub/music/LocalMusicPlugin.java
@@ -18,6 +18,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.provider.MediaStore;
 import android.util.Base64;
+import java.io.File;
 
 import androidx.activity.result.ActivityResult;
 
