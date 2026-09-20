e24c2e79b580d4ed0253930aec1e86315eb6127e
jxyang2025 <jxy5460@gmail.com>
2026-09-20 12:36:26 +0800

    chore: 删除functions目录，改为使用dist/_worker.js

--- functions/routes/api.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-import type { Env } from "../types/hono";
-import { rssRoutes } from "./podcast/rss";
-
-export const podcastRoutes = new Hono<{
-  Bindings: Env;
-}>();
-
-podcastRoutes.route("/rss", rssRoutes);
